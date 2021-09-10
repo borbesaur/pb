81019d9727bd738ba517c9acc3ba0ef0182af0e1
borbersaur <plasmabeach@gmail.com>
2021-09-10 00:36:40 -0400

    welcome to pb

---
+++ src/components/homepage/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="main">
+      <h1>Welcome To Plasma Beach</h1>
+
+      <p>The World’s first crowd-sourced, NFT-based cartoon show. </p>
+
+      <p>
+        Plasma Beach is a sleepy beachside community located in the caudate lobe
+        of some guy’s liver. All the happenings, all the drama, all the trials
+        and tribulations our characters will endure will all happen at the
+        microscopic level. Like the Magic School Bus or that one episode of Rick
+        and Morty. We already have the plot for 4 seasons of animated adventures
+        but we need you to help us build the narrative! (And raise the money)
+      </p>
+      <p>
+        All of our NFTs will double as membership cards to access our
+        stakeholders area in the discord server. All stakeholders will get a say
+        in how the story unfolds in Plasma Beach and will be able to offer
+        feedback in real time as the show is being made. Stakeholders will also
+        be rewarded with profit sharing at every phase where profit happens.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
