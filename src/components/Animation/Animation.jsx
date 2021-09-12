f51f9e332cca88ce9d40e4b2396b0c4e8fcc5d9c
borbersaur <plasmabeach@gmail.com>
2021-09-12 00:22:56 -0400

    dumb animation works both ways and dissappears

--- src/components/Animation/Animation.jsx
+++ src/components/Animation/Animation.jsx
@@ -5,48 +5,55 @@ import { useEffect, useState } from "react";
 const Animation = () => {
   const [right, setRight] = useState(0);
   const [top, setTop] = useState(-12);
+  const [visibility, setVisibility] = useState("visible");
   let go = true;
 
   const handleScroll = (e) => {
     const position = window.scrollY;
     if (position < 700) return;
 
-    if (position >= 700) {
-      const coord = (position - 700) / 7;
-      if (go) {
-        let topValue = -12;
-        let rightValue = coord > 0 ? coord : 0;
+    const coord = (position - 700) / 7;
+    if (go) {
+      let topValue = -12;
+      let rightValue = coord > 0 ? coord : 0;
 
-
-        if (rightValue >= 85 && top <= 8) {
-            const blah = position - 1340;
-            topValue = blah / 4;
-            if (topValue > 8) topValue = 8;
-        }
-
-
-        if(position > 1295 && position < 1372) {
-            rightValue = 85;
-        }  
-        if(position >=1372){
-            const diff = (position - 1372);
-            const factor = (diff / 7);
-            rightValue = 85 - factor + 1;
-            }
-
-        setTop(topValue);
-        setRight(rightValue);
-        go = false;
-        return;
+      if (rightValue >= 85 && top <= 8) {
+        console.log(position, rightValue, top);
+        const blah = position - 1340;
+        topValue = blah / 4;
+        if (topValue > 8) topValue = 8;
+        rightValue = 85;
       }
+
+      if (position >= 1372) {
+        console.log("this is happening", right);
+        const diff = position - 1372;
+        const factor = diff / 7;
+        if (right > 73) {
+          rightValue = 85 - factor + 1;
+        } else {
+          setVisibility("hidden");
+        }
+      } else {
+        setVisibility("visible");
+      }
+
+      setTop(topValue);
+      setRight(rightValue);
+      go = false;
+      return;
     }
   };
 
   window.addEventListener("scroll", handleScroll);
 
-    useEffect(()=>{
+  useEffect(
+    () => {
       go = true;
-    }, [right], [])
+    },
+    [right],
+    []
+  );
 
   return (
     <div className="animation-bg">
@@ -57,8 +64,8 @@ const Animation = () => {
           width: "200px",
           position: "absolute",
           right: `${right}vw`,
-          top: `${top}vw    `,
-          visibility: "visible",
+          top: `${top}vw`,
+          visibility: visibility,
         }}
       />
     </div>
